c04edc834d79ac1a8f91e8b2b671daf01396d4d1
delasimensah <mensadelasi@gmail.com>
2022-09-08 21:15:59 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-const App = () => {
-  return <div>Main App</div>;
-};
+// import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from "./components";
+import "./App.css";
+
+const App = () => (
+  <>
+    <Navbar />
+    {/* <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer /> */}
+  </>
+);
 
 export default App;
---
+++ src/components/Footer/FooterOverlay.jsx
@@ -0,0 +1,5 @@
+const FooterOverlay = () => {
+  return <div>FooterOverlay</div>;
+};
+
+export default FooterOverlay;
